7ea3df119a06335efc418349251013a06015a840
confusing1997 <luuhainam.it@gmail.com>
2020-09-06 20:28:51 +0700

    Creating Pages and Modifying navigation names

--- admin/includes/side_nav.php
+++ admin/includes/side_nav.php
@@ -2,16 +2,23 @@
     
     <ul class="nav navbar-nav side-nav">
         <li>
-            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i>Admin</a>
         </li>
+
         <li>
             <a href="users.php"><i class="fa fa-fw fa-bar-chart-o"></i> Users</a>
         </li>
+
         <li>
             <a href="upload.php"><i class="fa fa-fw fa-table"></i>Upload</a>
         </li>
+
         <li>
             <a href="comments.php"><i class="fa fa-fw fa-edit"></i>Comments</a>
+        </li>
+
+        <li>
+            <a href="photos.php"><i class="fa fa-fw fa-edit"></i>Photos</a>
         </li>
         
     </ul>
